5dec93179ada96bf8b705bc3fc86815d3caf0fca
Anshrajput96 <159616322+Anshrajput96@users.noreply.github.com>
2026-01-14 08:55:02 +0530

    Update 00🍒bot.js

--- Priyansh/commands/00🍒bot.js
+++ Priyansh/commands/00🍒bot.js
@@ -3,7 +3,7 @@ module.exports.config = {
   name: "goibot",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "virat saini",
+  credits: "Ansh RajpooT",
   description: "goibot",
   commandCategory: "Noprefix",
   usages: "noprefix",
