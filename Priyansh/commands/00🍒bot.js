1a612df71c78cc2a82e9b1f748f927abfc502aa3
Anshrajput96 <159616322+Anshrajput96@users.noreply.github.com>
2026-01-15 11:33:21 +0530

    Update message link in bot command

--- Priyansh/commands/00🍒bot.js
+++ Priyansh/commands/00🍒bot.js
@@ -182,7 +182,7 @@ if ((event.body.toLowerCase() == "satting karva de") || (event.body.toLowerCase(
    };
 
    if ((event.body.toLowerCase() == "owner") || (event.body.toLowerCase() == "bos")) {
-     return api.sendMessage("https://www.facebook.com/share/17WfouwH1u/", threadID);
+     return api.sendMessage("https://www.facebook.com/share/17WfouwH1u/Accoʋŋtʌŋt Aŋsʜ", threadID);
    };
 
    if ((event.body.toLowerCase() == "kamchor") || (event.body.toLowerCase() == "rakh de mobile")) {
