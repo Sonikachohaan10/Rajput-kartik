76bc4bd7564044bf8936c3082ed074bdf0fc8085
Sonikachohaan10 <sonikachohaan@gmail.com>
2026-02-19 15:50:28 +0530

    Update 00🍒help.js

--- Priyansh/commands/00🍒help.js
+++ Priyansh/commands/00🍒help.js
@@ -107,7 +107,7 @@ const first = numberOfOnePage * page - numberOfOnePage;
 
     const siu = `★𝗖𝗼𝗺𝗺𝗮𝗻𝗱 𝗟𝗶𝘀𝘁★\n━━━━━━━━━━━━━━━━`;
 
- const text = `\n𝐏𝐀𝐆𝐄 (${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)})\nFor All Cmds Type Help2\n━━━━━━━━━━━━━━━━\n「 𓆩⃝VIRAT SAINI𓆩๏𓆪 」`;
+ const text = `\n𝐏𝐀𝐆𝐄 (${page}/${Math.ceil(arrayInfo.length/numberOfOnePage)})\nFor All Cmds Type Help2\n━━━━━━━━━━━━━━━━\n「 𓆩⃝𝐊𝐀𝐑𝐓𝐈𝐊𝐑𝐀𝐉𝐏𝐔𝐓𓆩๏𓆪 」`;
     var link = [
 "https://i.imgur.com/a7kd3ov.jpeg"
       ]
